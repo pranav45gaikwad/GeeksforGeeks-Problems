02870cd99a3c640c510d9749f247a25f26340428
pranav45gaikwad <pranavgaikwad480@gmail.com>
2026-09-11 06:59:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print With Space/print-with-space.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String a = sc.next();
+        String b = sc.next();
+
+        // code here
+        System.out.println(a.concat(" ").concat(b));
+    }
+}
