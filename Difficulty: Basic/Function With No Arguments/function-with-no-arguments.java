72acfd9515ee1b676a05de15f45588a2ccc65f14
pranav45gaikwad <pranavgaikwad480@gmail.com>
2026-09-11 06:46:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With No Arguments/function-with-no-arguments.java
@@ -0,0 +1,20 @@
+// Write the helloFunction below to print "Hello"
+
+
+
+class Solution {
+    // code here
+    
+        public static void helloFunction()
+{
+    System.out.println("Hello");
+}
+
+    public static void main(String[] args)
+    {
+        Solution obj = new Solution();
+        obj.helloFunction();
+    }
+
+    
+}
