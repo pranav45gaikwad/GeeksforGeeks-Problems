23c239ad18a01f0d53b4aff1bb7885c97dc2a67f
pranav45gaikwad <pranavgaikwad480@gmail.com>
2026-09-11 06:44:58 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Function With Arguments/function-with-arguments.java
@@ -0,0 +1,19 @@
+// Write the complete argumentFunction below.
+// The function should take two arguments a and b
+// The function should return a+b
+class Solution {
+// code here
+
+public   int argumentFunction(int a,int b){
+   // System.out.print(a+b);
+    return a+b;
+}
+public static void main (String args[]) {
+
+        int a = 2;
+        int b = 3;
+
+Solution obj =  new Solution();
+obj.argumentFunction(a,b);
+}
+}
